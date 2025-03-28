c4c3ff12561cafc670a1f8367f8386d5f176ce2a
andymcgravy <andymcgravy@gmail.com>
2025-03-27 23:41:16 -0400

    finished get_args() in main

--- src/main.py
+++ src/main.py
@@ -14,3 +14,7 @@ def get_args():
     parser = ap.ArgumentParser(description="Resize images")
     parser.add_argument("input image path", description="The image to edit")
     parser.add_argument("output image path", description="The path to save the edited image to")
+    parser.add_argument("x", description="The x dimension to resize the image to")
+    parser.add_argument("y", description="The y dimension to resize the image to")
+    return parser.parse_args()
+
