37527657fd6159a106ff4f3b351c3cccb6191acc
andymcgravy <andymcgravy@gmail.com>
2025-03-31 22:11:31 -0400

    I need to redo the way I handled arguments, deleted it entirely!

--- src/main.py
+++ src/main.py
@@ -10,12 +10,3 @@ import argparse as ap
 
 #function to take in the arguments
 
-def get_args():
-    parser = ap.ArgumentParser(description="Resize_images")
-    parser.add_argument("input_image_path", help="The image to edit")
-    parser.add_argument("output_image_path", help="The path to save the edited image to")
-    parser.add_argument("x", type=int, help="The x dimension to resize the image to")
-    parser.add_argument("y", type=int, help="The y dimension to resize the image to")
-    return parser.parse_args()
-
-print(get_args())
